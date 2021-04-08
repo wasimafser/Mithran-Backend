6f8af3c27a43d72333dfb61a5a967d3d3b13943a
Wasim Afser <wasim.afser@gmail.com>
2021-04-08 20:28:40 +0530

    include static

--- mithran/urls.py
+++ mithran/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.static import static
 
 def flutter_redirect(request, resource):
     return serve(request, resource, settings.FLUTTER_WEB_DIR)
